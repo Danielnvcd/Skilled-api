03c43ec6127c68778de6e6514a44a180913903fc
Daniel Rivera <fetomarindaniel@gmail.com>
2026-02-22 03:57:58 -0600

    mejoras login

--- run.py
+++ run.py
@@ -3,6 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # Usando ssl_context='adhoc' para habilitar HTTPS localmente
-    # Nota: El navegador mostrará una advertencia de seguridad que deberás aceptar.
     app.run(host='0.0.0.0', port=5000, debug=False)
